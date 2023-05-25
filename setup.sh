4d07b1e0adb00e81af37c6e1c8e960ea1580cd7c
István Derda <isde@spreadshirt.net>
2023-05-25 23:32:47 +0200

    make setup.sh work on any machine

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ docker run -d -p 7474:7474 -p 7687:7687 \
 -e apoc.import.file.enabled=true \
 -e apoc.import.file.use_neo4j_config=true \
 -e NEO4J_PLUGINS=\[\"apoc\"\,\"graph-data-science\"\] \
---volume=$HOME/git/nosql2023/data:/data \
---volume=$HOME/git/nosql2023/import:/import \
---volume=$HOME/git/nosql2023/logs:/logs \
---volume=$HOME/git/nosql2023/plugins:/plugins \
---volume=$HOME/git/nosql2023/scripts:/scripts \
+--volume=$(pwd)/data:/data \
+--volume=$(pwd)/import:/import \
+--volume=$(pwd)/logs:/logs \
+--volume=$(pwd)/plugins:/plugins \
+--volume=$(pwd)/scripts:/scripts \
 neo4j:5.6.0 
 
 docker exec neo4j neo4j-admin dbms set-initial-password datenbanken
